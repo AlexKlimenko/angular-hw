5f8dc79739aeef10841968a0a1eb6e8b2426de2d
AlexKlimenko <klymenko8olexandr@gmail.com>
2019-10-23 22:16:26 +0300

    all done

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,137 @@
-import { interval, Observable } from 'rxjs'
-import { merge, skip, take } from 'rxjs/operators'
+import { interval, Observable } from 'rxjs';
+import { merge, skip, take } from 'rxjs/operators';
 
-const publisherFibonacci = Observable.create(observer => {
-  let counter = 0
-  let a = 0
-  let b = 1
-  let c = a
+// !1
+const publisherFibonacci = Observable.create((observer) => {
+  let counter = 0;
+  let a = 0;
+  let b = 1;
+  let c = a;
 
-  const fib = del => {
+  const fib = (del) => {
     setTimeout(() => {
-      observer.next(counter)
+      observer.next(counter);
       // tslint:disable-next-line:no-increment-decrement
-      counter++
+      counter++;
 
-      c = a + b
-      a = b
-      b = c
-      fib(b)
-    }, del * 1000)
+      c = a + b;
+      a = b;
+      b = c;
+      fib(b);
+      // tslint:disable-next-line:align
+    }, del * 1000);
+  };
+  fib(b);
+});
+
+publisherFibonacci.subscribe(
+  (val) => {
+    // tslint:disable-next-line:no-console
+    console.log(val);
+  },
+  (err) => {
+    // tslint:disable-next-line:no-console
+    console.log(err);
+  },
+  () => {
+    // tslint:disable-next-line:no-console
+    console.log('Done');
+    // tslint:disable-next-line:trailing-comma
   }
-  fib(b)
-})
+);
+
+// !2///////////////////////////////////
+const publisher1 = interval(100);
+const publisher2 = interval(300)
+  .pipe(merge(publisher1))
+  .pipe(skip(4))
+  .pipe(take(8));
+
+publisher2.subscribe((val) => {
+  // tslint:disable-next-line:no-console
+  console.log(val);
+});
+
+// !3//////////////////////////////////
+const observerInterval = (val) => {
+  Observable.create((observer) => {
+    let counter = 0;
+    setInterval(() => {
+      observer.next(counter);
+      // tslint:disable-next-line:no-increment-decrement
+      counter++;
+      // tslint:disable-next-line:align
+    }, val);
+  });
+};
+
+observerInterval(1000).subscribe(
+  (val) => {
+    // tslint:disable-next-line:no-console
+    console.log(val);
+  },
+  (err) => {
+    // tslint:disable-next-line:no-console
+    console.log(err);
+  },
+  () => {
+    // tslint:disable-next-line:no-console
+    console.log('Done');
+    // tslint:disable-next-line:trailing-comma
+  }
+);
+
+// !4///////////////////////////////////////
+// tslint:disable-next-line:ter-arrow-parens
+const observerFrom = (arr) =>
+  Observable.create((observer) => {
+    // tslint:disable-next-line:ter-arrow-parens
+    arr.forEach((el) => observer.next(el));
+  });
+
+observerFrom([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0]).subscribe(
+  (val) => {
+    // tslint:disable-next-line:no-console
+    console.log(val);
+  },
+  (err) => {
+    // tslint:disable-next-line:no-console
+    console.log(err);
+  },
+  () => {
+    // tslint:disable-next-line:no-console
+    console.log('Done');
+    // tslint:disable-next-line:trailing-comma
+  }
+);
+
+// !5//////////////////////////////////////////////////////////
+const observerFromWithDelay = (arr, val) =>
+  Observable.create((observer) => {
+    let index = 0;
+    setInterval(() => {
+      observer.next(arr[index]);
+      // tslint:disable-next-line:no-increment-decrement
+      index++;
+      if (index === arr.length) {
+        observer.complete();
+      }
+      // tslint:disable-next-line:align
+    }, val);
+  });
+
+observerFromWithDelay([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0], 1000).subscribe(
+  (val) => {
+    // tslint:disable-next-line:no-console
+    console.log(val);
+  },
+  (err) => {
+    // tslint:disable-next-line:no-console
+    console.log(err);
+  },
+  () => {
+    // tslint:disable-next-line:no-console
+    console.log('Done');
+    // tslint:disable-next-line:trailing-comma
+  }
+);
