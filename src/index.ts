229b00c1f823288d3840db1099d8e4ca7f0e8c25
AlexKlimenko <klymenko8olexandr@gmail.com>
2019-10-23 21:26:30 +0300

    start

--- src/index.ts
+++ src/index.ts
@@ -1,74 +1,23 @@
-enum Forecast {
-  sunny,
-  windy,
-  cloudy,
-  rainy,
-}
+import { interval, Observable } from 'rxjs'
+import { merge, skip, take } from 'rxjs/operators'
 
-class GetForecast {
-  public forecastList: any;
+const publisherFibonacci = Observable.create(observer => {
+  let counter = 0
+  let a = 0
+  let b = 1
+  let c = a
 
-  constructor(forecastList: any) {
-    this.forecastList = forecastList;
+  const fib = del => {
+    setTimeout(() => {
+      observer.next(counter)
+      // tslint:disable-next-line:no-increment-decrement
+      counter++
+
+      c = a + b
+      a = b
+      b = c
+      fib(b)
+    }, del * 1000)
   }
-
-  public getRandomForecast(): Promise<string> {
-    const index: number = this.getRandomInteger(0, (Object.values(this.forecastList).length / 2) - 1);
-    return new Promise<string>((resolve: (data: string) => any, reject: (err: string) => void) => {
-      if (!this.forecastList[index]) {
-        reject('This is error. Data is not defined');
-      }
-      setTimeout(() => {
-        resolve(this.forecastList[index]);
-        // tslint:disable-next-line:align
-      }, this.getRandomInteger());
-    });
-  }
-
-  public getFastestOfRandomForecasts(numOfElements: number) {
-    Promise.race(this.getPromiseList(numOfElements))
-      .then(res => console.log(`The fastest of random forecast is ${res}`))
-      .catch(err => console.log(err));
-  }
-
-  public getOftenOfRandomForecasts(numOfElements: number) {
-    Promise.all(this.getPromiseList(numOfElements))
-      .then((res) => {
-        return Object.values(res);
-      })
-      .then(res => this.sortByFrequency(res))
-      .then(res => console.log(`Most often repeated weather forecast is ${res[0]}`))
-      .catch(err => console.log(err));
-  }
-
-  private sortByFrequency(forecastList: any[]) {
-    const frequency: { [k: string]: number } = {};
-    forecastList.forEach(value => frequency[value] = 0);
-    // tslint:disable-next-line:no-increment-decrement
-    const uniques = forecastList.filter(value => ++frequency[value] === 1);
-    return uniques.sort((a, b) => (frequency[b] - frequency[a]));
-  }
-
-  private getPromiseList(numOfElements: number): any[] {
-    const arrOfPromises1 = [];
-    // tslint:disable-next-line:no-increment-decrement
-    for (let i = 0; i < numOfElements; i++) {
-      arrOfPromises1.push(this.getRandomForecast());
-    }
-    return arrOfPromises1;
-  }
-
-  private getRandomInteger(intervalStart: number = 500, intervalEnd: number = 3000): number {
-    return Math.round(intervalStart - 0.5 + Math.random() * (intervalEnd - intervalStart + 1));
-  }
-}
-
-const getForecast: GetForecast = new GetForecast(Forecast);
-
-getForecast.getRandomForecast()
-  .then((data: string) => {
-    console.log(data);
-  });
-
-getForecast.getFastestOfRandomForecasts(4);
-getForecast.getOftenOfRandomForecasts(40);
+  fib(b)
+})
