05a70f0aab3aa8fa458d9e2debae6eee6931a41c
AlexKlimenko <klymenko8olexandr@gmail.com>
2019-10-23 22:35:35 +0300

    all done

--- src/index.ts
+++ src/index.ts
@@ -1,126 +1,47 @@
-import { interval, Observable } from 'rxjs';
-import { merge, skip, take } from 'rxjs/operators';
+import { from, interval, of } from 'rxjs';
+import { bufferCount, concatMap, delay, filter, map } from 'rxjs/operators';
 
 // !1
-const publisherFibonacci = Observable.create((observer) => {
-  let counter = 0;
-  let a = 0;
-  let b = 1;
-  let c = a;
-
-  const fib = (del) => {
-    setTimeout(() => {
-      observer.next(counter);
+const publisher1 = interval(200)
+  .pipe(
+    filter((val) => {
+      if (val < 2) return false;
       // tslint:disable-next-line:no-increment-decrement
-      counter++;
-
-      c = a + b;
-      a = b;
-      b = c;
-      fib(b);
-      // tslint:disable-next-line:align
-    }, del * 1000);
-  };
-  fib(b);
-});
-
-publisherFibonacci.subscribe(
-  (val) => {
-    // tslint:disable-next-line:no-console
-    console.log(val);
-  },
-  (err) => {
-    // tslint:disable-next-line:no-console
-    console.log(err);
-  },
-  () => {
-    // tslint:disable-next-line:no-console
-    console.log('Done');
-    // tslint:disable-next-line:trailing-comma
-  }
-);
-
-// !2///////////////////////////////////
-const publisher1 = interval(100);
-const publisher2 = interval(300)
-  .pipe(merge(publisher1))
-  .pipe(skip(4))
-  .pipe(take(8));
-
-publisher2.subscribe((val) => {
-  // tslint:disable-next-line:no-console
-  console.log(val);
-});
-
-// !3//////////////////////////////////
-const observerInterval = (val) => {
-  Observable.create((observer) => {
-    let counter = 0;
-    setInterval(() => {
-      observer.next(counter);
-      // tslint:disable-next-line:no-increment-decrement
-      counter++;
-      // tslint:disable-next-line:align
-    }, val);
-  });
-};
-
-observerInterval(1000).subscribe(
-  (val) => {
-    // tslint:disable-next-line:no-console
-    console.log(val);
-  },
-  (err) => {
-    // tslint:disable-next-line:no-console
-    console.log(err);
-  },
-  () => {
-    // tslint:disable-next-line:no-console
-    console.log('Done');
-    // tslint:disable-next-line:trailing-comma
-  }
-);
-
-// !4///////////////////////////////////////
-// tslint:disable-next-line:ter-arrow-parens
-const observerFrom = (arr) =>
-  Observable.create((observer) => {
-    // tslint:disable-next-line:ter-arrow-parens
-    arr.forEach((el) => observer.next(el));
-  });
-
-observerFrom([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0]).subscribe(
-  (val) => {
-    // tslint:disable-next-line:no-console
-    console.log(val);
-  },
-  (err) => {
-    // tslint:disable-next-line:no-console
-    console.log(err);
-  },
-  () => {
-    // tslint:disable-next-line:no-console
-    console.log('Done');
-    // tslint:disable-next-line:trailing-comma
-  }
-);
-
-// !5//////////////////////////////////////////////////////////
-const observerFromWithDelay = (arr, val) =>
-  Observable.create((observer) => {
-    let index = 0;
-    setInterval(() => {
-      observer.next(arr[index]);
-      // tslint:disable-next-line:no-increment-decrement
-      index++;
-      if (index === arr.length) {
-        observer.complete();
+      for (let i = 2; i < val; i++) {
+        if (val % i === 0) return false;
       }
-      // tslint:disable-next-line:align
-    }, val);
-  });
+      return true;
+    }),
+  )
+  .pipe(bufferCount(2))
+  // tslint:disable-next-line:ter-arrow-parens
+  .pipe(map((val) => `${val[0] + val[1]}`));
 
-observerFromWithDelay([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0], 1000).subscribe(
+publisher1.subscribe(
+  (val) => {
+    // tslint:disable-next-line:no-console
+    console.log(val);
+  },
+  (err) => {
+    // tslint:disable-next-line:no-console
+    console.log(err);
+  },
+  () => {
+    // tslint:disable-next-line:no-console
+    console.log('Done');
+  },
+);
+
+// !2
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
+const publisher2 = from(arr).pipe(
+  bufferCount(3),
+  // tslint:disable-next-line:ter-arrow-parens
+  concatMap((val) => of(val).pipe(delay(2000))),
+);
+
+publisher2.subscribe(
   (val) => {
     // tslint:disable-next-line:no-console
     console.log(val);
@@ -133,5 +54,5 @@ observerFromWithDelay([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0], 1000).subscribe(
     // tslint:disable-next-line:no-console
     console.log('Done');
     // tslint:disable-next-line:trailing-comma
-  }
+  },
 );
