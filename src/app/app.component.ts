d6357aa8fe3db49a56c40948d7bf07d29f33be54
AlexKlimenko <klymenko8olexandr@gmail.com>
2019-11-03 22:21:17 +0200

    start custom pipe task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,44 +1,10 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { ImageUpdateService } from './services/image-update.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <h2>{{ headerText }}</h2>
-    <app-img [selectedPhoto]="galleryImg"></app-img>
-    <app-gallery [photos]="photos" (photoSrc)="handler($event)"></app-gallery>
-  `,
+  templateUrl: `./app.component.html`,
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, OnDestroy {
-  public headerText = 'Homework_7';
-  public photos: string[] = [];
-  public galleryImg: string;
-  private listener: Subscription;
-
-  constructor(private service: ImageUpdateService) {}
-
-  public subscribe(): void {
-    this.listener = this.service.getRandomData.subscribe((data: string) => {
-      this.galleryImg = data;
-    });
-  }
-
-  public handler($event: string): void {
-    this.listener.unsubscribe();
-    this.service.stopInterval();
-    this.galleryImg = $event;
-    this.subscribe();
-  }
-
-  ngOnInit(): void {
-    this.photos = this.service.getData();
-    this.galleryImg = this.photos[0];
-    this.subscribe();
-  }
-
-  ngOnDestroy(): void {
-    this.listener.unsubscribe();
-  }
+export class AppComponent {
+  constructor() {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { GalleryComponent } from './gallery/gallery.component';
-import { ImgComponent } from './img/img.component';
-import { ImageUpdateService } from './services/image-update.service';
 
 @NgModule({
-  declarations: [AppComponent, GalleryComponent, ImgComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule],
-  providers: [ImageUpdateService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
